8212c430d0523a35cbc761f66fd39b9ab5324eb8
Arya Dwi Putra <aryadptr.developer@gmail.com>
2024-06-10 15:03:39 +0700

    Create checkbox component

--- resources/js/Components/Dashboard/Checkbox.jsx
+++ resources/js/Components/Dashboard/Checkbox.jsx
@@ -1,4 +1,4 @@
-export default function Checkbox({ label, ...props }) {
+export default function Checkbox({ label, errors, ...props }) {
     return (
         <div>
             <div className="flex flex-row items-center gap-2">
@@ -8,6 +8,10 @@ export default function Checkbox({ label, ...props }) {
                     className={'rounded-md bg-white border-gray-200 dark:bg-gray-950 dark:border-gray-800 checked:bg-teal-500'}
                 />
                 <label className="text-sm text-gray-700 dark:text-gray-400">{label}</label>
+
+                {errors && (
+                    <small className='text-xs text-red-500'>{errors}</small>
+                )}
             </div>
         </div>
 
